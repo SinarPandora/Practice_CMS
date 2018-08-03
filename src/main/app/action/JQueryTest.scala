c5e7e3679e947cffead4cb11f1fd8bac2e142d41
SinarPandora <lindao1997@live.com>
2018-08-03 17:07:58 +0800

    bug fix
    Took 50 minutes

--- src/main/app/action/JQueryTest.scala
+++ src/main/app/action/JQueryTest.scala
@@ -1,5 +1,15 @@
 package app.action
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.{RequestMapping, ResponseBody}
+
+@Controller
+@RequestMapping(value = Array("appx/"))
 class JQueryTest {
 
+  @ResponseBody
+  @RequestMapping(Array("itWorks"))
+  def itJustWorks = "Hello World"
+
+  
 }
